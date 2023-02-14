86a14714b52addb13ac6041b27660c5bac25ae4e
Ali goudarzi <me74242@gmail.com>
2023-02-15 02:20:52 +0330

    dynamic The Message

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
 // React
-import React from "react";
+import React, { useState } from "react";
 // React
 // Notif
 import addNotification, { Notifications } from "react-push-notification";
+import { Options } from "react-push-notification/dist/notifications/Storage";
+
 // Notif
 
 const App = () => {
+  const [pushData, setPushData] = useState<Options>({
+    title: "",
+    subtitle: "",
+    message: "",
+    theme: "darkblue",
+    native: true,
+  });
+  const { title, subtitle, message } = pushData;
   return (
     <div>
       <Notifications />
+      <input
+        type="text"
+        onChange={(e) =>
+          setPushData((prevState) => ({ ...prevState, title: e.target.value }))
+        }
+        placeholder="Enter Title"
+      />{" "}
+      <input
+        type="text"
+        onChange={(e) =>
+          setPushData((prevState) => ({
+            ...prevState,
+            subtitle: e.target.value,
+          }))
+        }
+        placeholder="Enter subtitle"
+      />{" "}
+      <input
+        type="text"
+        onChange={(e) =>
+          setPushData((prevState) => ({
+            ...prevState,
+            message: e.target.value,
+          }))
+        }
+        placeholder="Enter message"
+      />
+      <br />
+      <br />
+      <br />
+      <br />
       <button
         onClick={() => {
           addNotification({
-            title: "Warning",
-            subtitle: "This is a subtitle",
-            message: "This is a very long message",
+            title,
+            subtitle,
+            message,
             theme: "darkblue",
             native: true, // when using native, your OS will handle theming.
           });
         }}
       >
-        Clik Me {":)"}
+        warning notif = darkblue
       </button>
     </div>
   );
