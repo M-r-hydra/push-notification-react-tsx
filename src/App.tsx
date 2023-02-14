40c768e2aea798ac7223f3339dc5b355454ca794
Ali goudarzi <me74242@gmail.com>
2023-02-15 02:07:19 +0330

    install notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+// React
 import React from "react";
+// React
+// Notif
+import addNotification, { Notifications } from "react-push-notification";
+// Notif
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Notifications />
+      <button
+        onClick={() => {
+          addNotification({
+            title: "Warning",
+            subtitle: "This is a subtitle",
+            message: "This is a very long message",
+            theme: "darkblue",
+            native: true, // when using native, your OS will handle theming.
+          });
+        }}
+      >
+        Clik Me {":)"}
+      </button>
+    </div>
+  );
 };
 
 export default App;
